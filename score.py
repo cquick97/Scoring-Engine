dc9df68dd3931369bbe348202540acbbc0000a70
Connor Quick <cquick197@gmail.com>
2014-12-11 21:37:34 -0500

    Added check for sudoers vulnerability
    The file /etc/sudoers.d/all allows all users to run sudo commands with no
    password.

--- score.py
+++ score.py
@@ -2,29 +2,39 @@
 
 # This is a scoring engine based on CyberPatriot.
 
+# Vulns:
+#   C99 Shell
+#   Sudo allows all users for no password
+
 import os.path
 import time
 
-def c99(file):
+def sudoers(report):
+    if os.path.isfile("/etc/sudoers.d/all") == False:
+        report.write("\n<li>Sudoers file secure\n")
+        print("Sudoers file secure")
+    return
+
+def c99(report):
     if os.path.isfile("/var/www/c99.php") == False:
-        file.write("\n<li>C99 Shell removed!\n")
+        report.write("\n<li>C99 Shell removed!\n")
         print("C99 Shell Removed!")
     return
 
 while True:
-    with open("/home/connor/scorereport.html", "w") as file:
-        file.write("<!DOCTYPE html>\n")
-        file.write("<head>\n")
-        file.write("<meta charset=\"UTF-8\">\n")
-        file.write("<title>Score Report</title>\n")
-        file.write("</head>\n\n")
-        file.write("<body>\n")
-        file.write("<center><b>Score Report</b></center>\n")
-        file.write("<ol>\n")
-        c99(file)
-        file.write("</ol>\n")
-        file.write("</body>\n")
-        file.write("</html>\n")
+    with open("/home/connor/scorereport.html", "w") as report:
+        report.write("<!DOCTYPE html>\n")
+        report.write("<head>\n")
+        report.write("<meta charset=\"UTF-8\">\n")
+        report.write("<title>Score Report</title>\n")
+        report.write("</head>\n\n")
+        report.write("<body>\n")
+        report.write("<center><b>Score Report</b></center>\n")
+        report.write("<ol>\n")
+        c99(report)
+        report.write("</ol>\n")
+        report.write("</body>\n")
+        report.write("</html>\n")
     time.sleep(10)
 
 
